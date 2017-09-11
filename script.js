0dfe51b7890502e95fbf92e390258bcc14783f3c
Dylan <dylan.zou.schupp@gmail.com>
2017-09-10 22:23:25 -0700

    inits game arrays

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+let choices = ['∑', 'π', '®', '≠', 'ß', '∞', 'α', 'µ'];
+let pairs = [16];
+
+function initGame() {
+  for (let i = 0; i < 16; i++){
+    pairs[i] = 0;
+  }
+}
 $(document).ready(function() {
-      $(".tile").on('click', function() {
-        $(this).find(".tile-cover").css("display", "none");
-      });
-    });
+  initGame();
+  $(".tile").on('click', function() {
+    $(this).find(".tile-cover").css("display", "none");
+  });
+});
